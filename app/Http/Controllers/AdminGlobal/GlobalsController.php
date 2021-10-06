7d2b833207e16d52e1e67c766fa97e587610bd2b
Johan Jose Villavicencio Valles <Johan.VV@VirreysolisIPS.loc>
2021-10-06 15:09:00 -0500

    up fix x2

--- app/Http/Controllers/AdminGlobal/GlobalsController.php
+++ app/Http/Controllers/AdminGlobal/GlobalsController.php
@@ -187,7 +187,7 @@ class GlobalsController extends Controller
     /* PERMISOS PARA EL SIDEBAR */
     public function getMenuDash(Request $request)
     {
-        $countSedes  = SedSede::count();
+        $countSedes  = 60/* SedSede::count() */;
         $countSuc    = Sucursal::count();
         $countServ   = Servicios::count();
         $countGrupos = Grupos::count();
