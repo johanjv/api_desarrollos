a5b2ba011fe6a8b88de3a24b2536968546811fc0
Johan Jose Villavicencio Valles <Johan.VV@VirreysolisIPS.loc>
2021-12-16 15:00:16 -0500

    up recaptcha

--- app/Http/Controllers/CertificadosEscolares/CertificadosEscolaresController.php
+++ app/Http/Controllers/CertificadosEscolares/CertificadosEscolaresController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\CertificadosEscolares;
 use App\Http\Controllers\Controller;
 use App\Models\CertificadosEscolares\AfiliadosEscolares;
 use Illuminate\Http\Request;
+use DB;
 
 class CertificadosEscolaresController extends Controller
 {
     public function generarPdf(Request $request)
     {
         $dataAfiliado = AfiliadosEscolares::where('Documento', $request["doc"])->first();
+        DB::connection('sqlsrv')->table('ESCOLAR.HISTORIAL_CERT_ESCOLAR')->insert([
+            "DOC"   => $request["doc"],
+            "IP"    => $request->ip(),
+            "FECHA" => date("Y-m-d h:i:s")
+        ]);
+
         return response()->json(["dataAfiliado" => $dataAfiliado], 200);
     }
+
 }
