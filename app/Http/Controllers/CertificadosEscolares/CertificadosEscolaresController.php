26813e12e354f195bc00d8c12d22d150b3319a81
Johan Jose Villavicencio Valles <Johan.VV@VirreysolisIPS.loc>
2021-12-20 17:24:21 -0500

    solo se almacene historial de descarga si encuentra al afiliado

--- app/Http/Controllers/CertificadosEscolares/CertificadosEscolaresController.php
+++ app/Http/Controllers/CertificadosEscolares/CertificadosEscolaresController.php
@@ -12,11 +12,13 @@ class CertificadosEscolaresController extends Controller
     public function generarPdf(Request $request)
     {
         $dataAfiliado = AfiliadosEscolares::where('Documento', $request["doc"])->first();
-        DB::connection('sqlsrv')->table('ESCOLAR.HISTORIAL_CERT_ESCOLAR')->insert([
-            "DOC"   => $request["doc"],
-            "IP"    => $request->ip(),
-            "FECHA" => date("Y-m-d h:i:s")
-        ]);
+        if ($dataAfiliado) {
+            DB::connection('sqlsrv')->table('ESCOLAR.HISTORIAL_CERT_ESCOLAR')->insert([
+                "DOC"   => $request["doc"],
+                "IP"    => $request->ip(),
+                "FECHA" => date("Y-m-d h:i:s")
+            ]);
+        }
 
         return response()->json(["dataAfiliado" => $dataAfiliado], 200);
     }
