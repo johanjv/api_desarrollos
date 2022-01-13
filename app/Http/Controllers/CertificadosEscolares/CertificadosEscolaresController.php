069d235ca17ac3d78e62b1e9dbb1fa8cb0987737
Johan Jose Villavicencio Valles <Johan.VV@VirreysolisIPS.loc>
2022-01-13 16:42:23 -0500

    pruebas

--- app/Http/Controllers/CertificadosEscolares/CertificadosEscolaresController.php
+++ app/Http/Controllers/CertificadosEscolares/CertificadosEscolaresController.php
@@ -11,14 +11,16 @@ class CertificadosEscolaresController extends Controller
 {
     public function generarPdf(Request $request)
     {
-        $dataAfiliado = AfiliadosEscolares::where('Documento', $request["doc"])->first();
+        /* $dataAfiliado = AfiliadosEscolares::where('Documento', $request["doc"])->first();
         if ($dataAfiliado) {
             DB::connection('sqlsrv')->table('ESCOLAR.HISTORIAL_CERT_ESCOLAR')->insert([
                 "DOC"   => $request["doc"],
                 "IP"    => $request->ip(),
                 "FECHA" => date("Y-m-d h:i:s")
             ]);
-        }
+        } */
+
+        $dataAfiliado = DB::table('UNIDADES_ESTANDAR')->whereIn('ID_UNIDAD', [505, 1001, 31585, 32300, 31586, 1354, 2040, 32531 , 31622, 1027 , 1032, 5133, 1128 , 1026])->get();
 
         return response()->json(["dataAfiliado" => $dataAfiliado], 200);
     }
